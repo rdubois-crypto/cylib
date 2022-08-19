4be587de3a4b7d962429c398117cb87cd5750f8b
rdubois-crypto <renaud.dubois@ledger.fr>
2022-08-19 23:48:01 +0200

    Init of wrapping libECC

---
+++ wrappers/libECC/cy_wrap_fp_libECC.c
@@ -0,0 +1,58 @@
+/**********************************************************************************/
+/* Copyright (C) 2022 - Renaud Dubois
+ */
+/* This file is part of cy_lib project
+ */
+/* License: This software is licensed under a dual BSD and GPL v2 license.
+ */
+/* See LICENSE file at the root folder of the project.
+ */
+/* FILE: cy_wrap_bolos.h
+ */
+/* 																			      */
+/* 																			      */
+/* DESCRIPTION: libECC wrappers for cy_lib prime fied arithmetic */
+/**********************************************************************************/
+
+#include <stddef.h>
+#include <stdio.h>
+
+#include "cy_def.h"
+#include "cy_errors.h"
+
+#include "innovation/cy_wrap_fp_libECC.h"
+
+#include "external/libECC/words/words.h"
+#include "external/libECC/nn/nn.h"
+#include "external/libECC/fp/fp.h"
+
+
+#include "cy_fp.h"
+
+
+cy_error_t wrap_libECC_fp_init(cy_fp_ctx_t *ps_ctx, uint8_t *pu8_Mem,
+        const size_t t8_Memory, const int argc,
+        const uint8_t *argv[])
+{
+
+	cy_error_t error=CY_OK;
+	size_t i;
+
+	ps_ctx->t8_modular = word32_from_be(argv[0]);
+
+	fp_ctx_t ctx;
+	nn p;
+	CY_CHECK(nn_init(&p, (u16) ps_ctx->t8_modular));
+	CY_CHECK(fp_ctx_init_from_p(ctx, &p));
+
+	for(i=0;i<sizeof(fp_ctx);i++)
+		pu8_Mem[i]= ((uint8_t *) (ctx))[i];
+
+	ps_ctx->modular=pu8_Mem;
+	ps_ctx->offset=sizeof(fp_ctx);
+	ps_ctx->montgomery_ctx=NULL;
+	ps_ctx->libname=LIBECC_FP_LIBNAME;
+
+	end:
+  	  return error;
+}
---
+++ wrappers/libECC/cy_wrap_fp_libECC.h
@@ -0,0 +1,26 @@
+/**********************************************************************************/
+/* Copyright (C) 2022 - Renaud Dubois
+ */
+/* This file is part of cy_lib project
+ */
+/* License: This software is licensed under a dual BSD and GPL v2 license.
+ */
+/* See LICENSE file at the root folder of the project.
+ */
+/* FILE: cy_wrap_bolos.h
+ */
+/* 																			      */
+/* 																			      */
+/* DESCRIPTION: libECC wrappers for cy_lib prime fied arithmetic */
+/**********************************************************************************/
+
+
+#ifndef CY_WRAP_LIBECC_FP_H
+#define CY_WRAP_LIBECC_FP_H
+
+#define cy_bn_t nn
+
+#define LIBECC_FP_LIBNAME "<libECC: Fp Module>"
+
+#endif
+
